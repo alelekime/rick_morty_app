44e99c7b6f1cb0c7d4755a236c3a9a9c2abc716d
Alessandra Souza <42846638+alelekime@users.noreply.github.com>
2026-03-17 17:37:24 -0300

    chore: initialize project and base folder structure

---
+++ rick_morty/rick_mortyApp.swift
@@ -0,0 +1,17 @@
+//
+//  rick_mortyApp.swift
+//  rick_morty
+//
+//  Created by Alessandra Souza da Silva on 17/03/26.
+//
+
+import SwiftUI
+
+@main
+struct rick_mortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ rick_mortyTests/rick_mortyTests.swift
@@ -0,0 +1,17 @@
+//
+//  rick_mortyTests.swift
+//  rick_mortyTests
+//
+//  Created by Alessandra Souza da Silva on 17/03/26.
+//
+
+import Testing
+@testable import rick_morty
+
+struct rick_mortyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
