4e51ec2a96f9574a702dac29fc42ed37959d97b0
Alessandra Souza <42846638+alelekime@users.noreply.github.com>
2026-03-18 01:41:57 -0300

    fix: add identifiable and id to conform to protocol

--- rick_morty/Features/Model/CharacterStatus.swift
+++ rick_morty/Features/Model/CharacterStatus.swift
@@ -5,9 +5,11 @@
 //  Created by Alessandra Souza da Silva on 18/03/26.
 //
 
-enum CharacterStatus: String, CaseIterable {
+enum CharacterStatus: String, CaseIterable, Identifiable {
     case all
     case alive
     case dead
     case unknown
+
+    var id: String { rawValue }
 }
