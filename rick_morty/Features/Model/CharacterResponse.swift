6a33038af49f4a23aee44a3d5235d359ea9602cb
Alessandra Souza <42846638+alelekime@users.noreply.github.com>
2026-03-17 21:22:16 -0300

    feat: add character response api wrapper

---
+++ rick_morty/Features/Model/CharacterResponse.swift
@@ -0,0 +1,19 @@
+//
+//  CharacterResponse.swift
+//  rick_morty
+//
+//  Created by Alessandra Souza da Silva on 17/03/26.
+//
+
+// API Resposnse
+struct CharacterResponse: Decodable {
+    let info: [String: Int]
+    let results: [Character]
+}
+
+struct CharacterResponseInfo: Decodable {
+    let count: Int
+    let pages: Int
+    let next: String?
+    let prev: String?
+}
